faa2ebc62ea8231ff4a8abe6cc4e5bb5575cbe07
CISSE Fatoumata <fatou.ycisse@gmail.com>
2021-09-21 13:57:31 +0200

    models

--- backend/models/password.js
+++ backend/models/password.js
@@ -1,16 +1,14 @@
 const passwordValidator = require('password-validator');
 
-//creation schema
 let schema = new passwordValidator();
 
-//ajout des propriétes
 schema
-    .is().min(6) //longueur min 6
-    .is().max(50) //longuer max 50
-    .has().uppercase() //doit avoir au moins une majuscule
-    .has().lowercase() // doit avoir au moins une miniscule
-    .has().digits(1) // doit avoir au moins 1chiffre
-    .has().not().spaces() //ne doit pas avoir d'espaces
-    .is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist de valeurs à proscrire
+    .is().min(6)
+    .is().max(50)
+    .has().uppercase()
+    .has().lowercase()
+    .has().digits(2)
+    .has().not().spaces()
+    .is().not().oneOf(['Passw0rd', 'Password123']);
 
 module.exports = schema;
